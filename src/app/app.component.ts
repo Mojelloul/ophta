922bf13c761d19515fca0a0d96424247cbee8336
Mojelloul <jelloulmohamed11@gmail.com>
2020-06-26 03:22:22 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgxCsvParser, NgxCSVParserError } from 'ngx-csv-parser';
+import { Ophta } from './ophta';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ophta';
+  csvRecords:Ophta={
+    id:0,
+    maladie:'',
+    prix:0,
+    facture:'', 
+    dateFacturation: '',
+  }
+  header = true;
+ 
+  constructor(private ngxCsvParser: NgxCsvParser) {
+  }
+ 
+  @ViewChild('fileImportInput', { static: false }) fileImportInput: any;
+ 
+  // Your applications input change listener for the CSV File
+  fileChangeListener($event: any): void {
+ 
+    // Select the files from the event
+    const files = $event.srcElement.files;
+ 
+    // Parse the file you want to select for the operation along with the configuration
+    this.ngxCsvParser.parse(files[0], { header: this.header, delimiter: ',' })
+      .pipe().subscribe((result: any) => {
+ 
+        console.log('Result', result);
+        this.csvRecords = result;
+      }, (error: NgxCSVParserError) => {
+        console.log('Error', error);
+      });
+  }
+  facturee(csvData){
+   if(csvData.facture=='f') 
+   {
+   csvData.facture='n'
+   csvData.dateFacturation='';
+
+   }
+   else{
+   csvData.facture='f'
+   csvData.dateFacturation=Date.now().toString()
+}
+  }
 }
---
+++ src/app/ophta.ts
@@ -0,0 +1,7 @@
+export interface Ophta {
+    id?:number,
+    maladie?:string,
+    prix?:number,
+    facture?:string,
+    dateFacturation?:string,
+}
